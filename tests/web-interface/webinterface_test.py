0ad15e68d980f99cd3a886ff5944a5fb9e0d177d
Nicholas Smith <nicholassmith@glaroam2-148-70.wireless.gla.ac.uk>
2018-03-08 18:53:27 +0000

    user settings input boxes (NO LOGIC), photomask upload

--- tests/web-interface/webinterface_test.py
+++ tests/web-interface/webinterface_test.py
@@ -1,16 +1,50 @@
-from flask import Flask, render_template
-import datetime
-app = Flask(__name__)
+import os
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
-@app.route("/")
+
+UPLOAD_FOLDER = '/Users/nicholassmith/Documents/Work/UNAAAY/YEAR5/TDP5'
+ALLOWED_EXTENSIONS = set(['bmp', 'ai', 'eps', 'pdf', 'svg'])
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=['GET', 'POST'])
 def main():
-   now = datetime.datetime.now()
-   timeString = now.strftime("%Y-%m-%d %H:%M")
-   templateData = {
-      'title' : 'HELLO!',
-      'time': timeString
-      }
-   return render_template('main2.html', **templateData)
+
+    # get relevant info from user
+    templateData = {'exposureTime' : '10','iterations': '50','substrateDiameter': '100'}
+
+    # TODO file name isnt showing up, fix this
+    if request.method == 'POST':
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            print('No file part')
+            return redirect(request.url)
+        
+        file = request.files['file']
+
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == '':
+            print('No selected file')
+            return redirect(request.url)
+
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+
+    return render_template('main2.html', templateData = templateData)
+
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=80, debug=True)
