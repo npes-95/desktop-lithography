b4252a8ce8d98aa6071555e57f089feac52c9e12
npes-95 <npe.smith.95@gmail.com>
2018-03-12 18:39:22 +0000

    web interface photomask upload working

--- tests/web-interface/webinterface_test.py
+++ tests/web-interface/webinterface_test.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from werkzeug.utils import secure_filename
 
 
@@ -21,30 +21,38 @@ def main():
     # get relevant info from user
     templateData = {'exposureTime' : '10','iterations': '50','substrateDiameter': '100'}
 
-    # TODO file name isnt showing up, fix this
     if request.method == 'POST':
 
         # check if the post request has the file part
         if 'file' not in request.files:
-            print('No file part')
+            flash('No file selected.', 'danger')
             return redirect(request.url)
         
         file = request.files['file']
 
-        # if user does not select file, browser also
-        # submit a empty part without filename
+        print(file.filename)
+
         if file.filename == '':
-            print('No selected file')
+            flash('No file selected.', 'danger')
             return redirect(request.url)
 
         if file and allowed_file(file.filename):
+
+            #this is where we want to pass the file to the DMD/processing functions
+
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            flash('New photomask ({}) added!'.format(filename), 'success')
+            return redirect(request.url)
+
+        else:
+            flash('Incompatible file format. Please try again.', 'danger')
+            return redirect(request.url)
 
     return render_template('main2.html', templateData = templateData)
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port=80, debug=True)
+    app.secret_key = 'super secret key'
+    app.config['SESSION_TYPE'] = 'filesystem'
+    app.run(host='0.0.0.0', port=80, debug=True)
