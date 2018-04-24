041ca24e398d38fa398b1cb8fa43cc66e280690f
npes-95 <npe.smith.95@gmail.com>
2018-04-24 18:08:45 +0000

    added setup gcode to init

--- lib/helper/TinyG.py
+++ lib/helper/TinyG.py
@@ -27,7 +27,13 @@ class MotorDriver():
         self.x = 0
         self.y = 0
         self.z = 0
-       
+        
+        # init motors: set scale to mm, assign m1 = x, m2 = y, m3 = z, set lead screw pitch (0.9mm), set microsteps, set polarity, set power management (z motor always on to hold dmd in place), set max speed
+        initGCode = "G21 \n$1ma=0 \n$2ma=1 \n$3ma=2 \n$1SA=0.9 \n$2SA=0.9 \n$3SA=0.9 \n$4SA=0.9 \n$1tr=8 \n$2tr=8 \n$3tr=8 \n$4tr=8 \n$1mi=8 \n$2mi=8 \n$3mi=8 \n$4mi=8 \n$1po=0 \n$2po=0 \n$3po=0 \n$4po=0 \n$3pm=1 \n$xvm=1000 \n$yvm=1000 \n$zvm=1000"
+		self.textWrapper.write(initGCode)
+		self.textWrapper.flush()
+		
+		
     def __del__(self):
         self.ser.close()
         
@@ -45,13 +51,7 @@ class MotorDriver():
         
     def moveToCoordinates(self):
         
-        gcode = "G1 f400 X" + str(self.x)
-        self.writeGCodeLine(gcode)
-        
-        gcode = "G1 f400 Y" + str(self.y)
-        self.writeGCodeLine(gcode)
-        
-        gcode = "G1 f400 Z" + str(self.z)
+        gcode = "G0 X" + str(self.x) + " Y" + str(self.y) + " Z" + str(self.z)
         self.writeGCodeLine(gcode)
         
         
